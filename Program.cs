c2ae6712a07dc1d576aa824026c33203bf4954e2
mbolka <31479820+mbolka@users.noreply.github.com>
2022-12-30 13:55:49 +0000

    test

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("AppConfig");
 builder.Configuration.AddAzureAppConfiguration(options =>
                     options.Connect(new Uri("https://bolek-app-configuration.azconfig.io"), new ManagedIdentityCredential()));
 builder.Services.Configure<WebApi.Flags.Settings>(builder.Configuration.GetSection("TestApp:Settings"));
